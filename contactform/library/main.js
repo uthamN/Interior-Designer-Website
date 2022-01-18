07d0a61573dbcf4a38f7fe9dfa8021fb31a4921c
uthamN <69818349+uthamN@users.noreply.github.com>
2022-01-18 19:38:38 +0530

    Add files via upload

---
+++ contactform/library/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('.btn').click(function(event){
+        console.log('clicked button')
+        var fname = $('.fname').val()
+        var lname = $('.lname').val()
+        var fname = $('.fname').val()
+        var number = $('.number').val()
+        var email = $('.email').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+        
+        if(fname.length>=5){   
+        }else{
+            statusElm.append('<div>Fname is not valid</div>')
+        }
+        if(lname.length>=1){   
+        }else{
+            statusElm.append('<div>lname is not valid</div>')
+        }
+        if(number = 10){
+
+        }else{
+            statusElm.append('<div>number is not valid</div>')
+
+        }
+
+        if(email.length >5 && email.includes('@') && email.includes('.')){
+
+        }else {
+            event.preventDeafault()
+            statusElm.append('<div>Email is not valid</div>')
+           }
+        if(message.length > 20){
+        }else {
+            event.preventDeafault()
+            statusElm.append('<div>Message is not valid</div>')
+        }
+          
+    });
+})
